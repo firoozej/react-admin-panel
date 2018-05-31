7568060d82678a2afab84f261e11e54049ec8fa3
Firooze J <firoozej2@gmail.com>
2018-05-31 12:13:44 +0430

    refactor creating nav

--- src/components/AppSidebarNav.js
+++ src/components/AppSidebarNav.js
@@ -28,18 +28,14 @@ const filterChildren = (item, names) => {
 const menu = (data) => {
     const names = data.nav.map(item => item.name);
     let items = navigation.items.reduce((acc, item) => {
-        let hasPermission = item.type === 'item' && names.includes(item.url.slice(1));
-        if (hasPermission) {
-            if (item.children) {
-                filterChildren(item, names);
-            }
-            return (item.children && item.children.length === 0) ? acc : acc.concat(item);
-        }
-        else if (item.children) {
+        let needsPermission = item.type === 'item';
+        let hasPermission = item.url && names.includes(item.url.slice(1));
+        if(item.children) {
             filterChildren(item, names);
             return (item.children.length === 0) ? acc : acc.concat(item);
         }
-        else return acc.concat(item);
+        if(!needsPermission || hasPermission) return acc.concat(item);
+        else return acc;
     }, []);
     return { items };
 }
